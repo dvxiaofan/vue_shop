7e2d03c30aec6c74f46ef99a859514e2e629e42e
xiaofan <1353815633@qq.com>
2020-03-26 21:53:33 +0800

    配置登录页面路由

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    'no-console': 'off',
+    'no-debugger': 'off',
+    quotes: 'single'
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ]
 }
