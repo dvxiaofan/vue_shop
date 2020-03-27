b8bba08d16014c62f5413554b27c8ced4e89200c
xiaofan <1353815633@qq.com>
2020-03-27 13:37:02 +0800

    路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -24,4 +24,19 @@ const router = new VueRouter({
   routes
 })
 
+// 挂载路由导航守卫
+/*
+  to: 将要访问的路径
+  from: 从哪个路径而来
+  next: 是一个函数, 表示放行
+*/
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 没有token, 强制跳转到登录页 (可以再加上服务器发送验证是否是正确的token)
+  if (!tokenStr) return next('/login')
+  next()
+})
+
 export default router
