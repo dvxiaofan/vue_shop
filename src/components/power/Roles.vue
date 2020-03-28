418542d5fdb2bd3e1cc7de847c322e1be73b2ea9
xiaofan <1353815633@qq.com>
2020-03-28 17:05:59 +0800

    添加角色功能实现

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -1,11 +1,145 @@
 <template>
   <div>
-    <h3>角色列表组件</h3>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <!-- 添加按钮区 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary" @click="addDialogVisible = true"
+            >添加角色</el-button
+          >
+        </el-col>
+      </el-row>
+      <!-- 角色列表区 -->
+      <el-table :data="rolesList" border stripe>
+        <!-- 展开列 -->
+        <el-table-column type="expand"></el-table-column>
+        <!-- 索引列 -->
+        <el-table-column type="index" prop="" label=""> </el-table-column>
+        <el-table-column label="角色名称" prop="roleName"> </el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"> </el-table-column>
+        <el-table-column label="操作" width="300px">
+          <template slot-scope="">
+            <el-button type="primary" size="mini" icon="el-icon-edit"
+              >编辑</el-button
+            >
+            <el-button type="danger" size="mini" icon="el-icon-delete"
+              >删除</el-button
+            >
+            <el-button type="warning" size="mini" icon="el-icon-setting"
+              >分配权限</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+
+    <!-- 添加角色弹出对话框 -->
+    <el-dialog
+      title="添加角色"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      @close="addDialogClosed"
+    >
+      <!-- 对话框内容主体 -->
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="80px"
+      >
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="addForm.roleName"></el-input>
+        </el-form-item>
+
+        <el-form-item label="角色描述" prop="roleDesc">
+          <el-input type="roleDesc" v-model="addForm.roleDesc"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 对话框底部区域 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addRole">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 所有角色
+      rolesList: [],
+      addDialogVisible: false,
+      addForm: {
+        roleName: '',
+        roleDesc: ''
+      },
+      addFormRules: {
+        roleName: [
+          { required: true, message: '请输入角色名', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '角色名长度在3--10个字符',
+            trigger: 'blur'
+          }
+        ],
+        roleDesc: [
+          { required: true, message: '请输入角色描述', trigger: 'blur' },
+          {
+            min: 6,
+            max: 20,
+            message: '角色描述长度在6--20个字符',
+            trigger: 'blur'
+          }
+        ]
+      }
+    }
+  },
+  created() {
+    this.getRolesList()
+  },
+  methods: {
+    async getRolesList() {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('后去角色列表失败')
+      }
+
+      this.rolesList = res.data
+    },
+    // 监听添加用户对话框的关闭
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields()
+    },
+    // 添加角色
+    addRole() {
+      this.$refs.addFormRef.validate(async valid => {
+        if (!valid) return false
+
+        // 发起添加角色请求
+        const { data: res } = await this.$http.post('roles', this.addForm)
+        if (res.meta.status !== 201) {
+          return this.$message.error('角色添加失败')
+        }
+
+        console.log('res: ', res)
+
+        this.$message.success('角色添加成功')
+        this.addDialogVisible = false
+        this.getRolesList()
+      })
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
