d25565fc0b42e69b3553a7b6f06ee1095335bcd1
xiaofan <1353815633@qq.com>
2020-03-31 22:25:00 +0800

    添加分类

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -12,7 +12,9 @@
       <!-- 添加按钮区 -->
       <el-row>
         <el-col class="btn-container">
-          <el-button type="primary">添加分类</el-button>
+          <el-button type="primary" @click="showAddCateDialog"
+            >添加分类</el-button
+          >
         </el-col>
         <!-- 表格区域 -->
         <tree-table
@@ -71,6 +73,39 @@
         </el-pagination>
       </el-row>
     </el-card>
+
+    <!-- 添加分类对话框 -->
+    <el-dialog
+      title="添加分类"
+      :visible.sync="addCateDialogVisible"
+      width="50%"
+      @close="addCateDialogClosed"
+    >
+      <el-form
+        label-width="100px"
+        :model="addCateForm"
+        :rules="addCateFormRules"
+        ref="addCateFormRef"
+      >
+        <el-form-item label="分类名称:" prop="cat_name">
+          <el-input v-model="addCateForm.cat_name"></el-input>
+        </el-form-item>
+        <el-form-item label="父级分类:">
+          <el-cascader
+            v-model="selectedKeys"
+            :options="parentCateList"
+            :props="cascaderProps"
+            @change="parentCateChanged"
+            clearable
+            change-on-select
+          ></el-cascader>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -113,7 +148,35 @@ export default {
           // 要使用的模板
           template: 'opt'
         }
-      ]
+      ],
+      // 控制添加分类对话框的显示与隐藏
+      addCateDialogVisible: false,
+      // 添加分类的表单数据对象
+      addCateForm: {
+        cat_name: '',
+        cat_pid: 0,
+        cat_level: 0
+      },
+      addCateFormRules: {
+        cat_name: [
+          {
+            required: true,
+            message: '请输入分类名称',
+            trigger: 'blur'
+          }
+        ]
+      },
+      // 父级分类列表
+      parentCateList: [],
+      // 指定级联选择器的配置对象
+      cascaderProps: {
+        expandTrigger: 'hover',
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      },
+      // 选中的父级分类ID数组
+      selectedKeys: []
     }
   },
   created() {
@@ -139,6 +202,59 @@ export default {
     handleCurrentChange(newPage) {
       this.queryInfo.pagenum = newPage
       this.getCateList()
+    },
+    showAddCateDialog() {
+      this.getParentCateList()
+      this.addCateDialogVisible = true
+    },
+    // 获取父级分类的数据列表
+    async getParentCateList() {
+      const { data: res } = await this.$http.get('categories', {
+        params: { type: 2 }
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取父级分类失败')
+      }
+
+      this.parentCateList = res.data
+    },
+    // 选择项发生变化触发事件
+    parentCateChanged() {
+      // 如果selectedKeys 的长度大于0 ,说明选中了父级分类
+      if (this.selectedKeys.length > 0) {
+        // 父级分类的ID
+        this.addCateForm.cat_pid = this.selectedKeys[
+          this.selectedKeys.length - 1
+        ]
+        // 为当前分类的等级复制
+        this.addCateForm.cate_level = this.selectedKeys.length
+        return false
+      } else {
+        this.addCateForm.cat_pid = 0
+        this.addCateForm.cat_level = 0
+      }
+    },
+    // 添加分类确定事件
+    addCate() {
+      this.$refs.addCateFormRef.validate(async valid => {
+        if (!valid) return false
+        const { data: res } = await this.$http.post(
+          'categories',
+          this.addCateForm
+        )
+        if (res.meta.status !== 201) {
+          return this.$message.error('添加分类失败')
+        }
+        this.$message.success('添加分类成功')
+        this.getCateList()
+        this.addCateDialogVisible = false
+      })
+    },
+    addCateDialogClosed() {
+      this.$refs.addCateFormRef.resetFields()
+      this.selectedKeys = []
+      this.addCateForm.cat_level = 0
+      this.addCateForm.cat_pid = 0
     }
   }
 }
@@ -147,5 +263,8 @@ export default {
 <style lang="less" scoped>
 .btn-container {
   margin-bottom: 15px;
+}
+.el-cascader {
+  width: 100%;
 }
 </style>
