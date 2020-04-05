37b6dee43e4eaaa2814919a70971ce391fe66a9b
xiaofan <1353815633@qq.com>
2020-04-05 14:53:05 +0800

    删除参数

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -59,7 +59,11 @@
                   @click="showEditDialog(scope.row.attr_id)"
                   >编辑</el-button
                 >
-                <el-button type="danger" icon="el-icon-delete" size="mini"
+                <el-button
+                  type="danger"
+                  icon="el-icon-delete"
+                  size="mini"
+                  @click="deleteParams(scope.row)"
                   >删除</el-button
                 >
               </template>
@@ -93,7 +97,11 @@
                   @click="showEditDialog(scope.row.attr_id)"
                   >编辑</el-button
                 >
-                <el-button type="danger" icon="el-icon-delete" size="mini"
+                <el-button
+                  type="danger"
+                  icon="el-icon-delete"
+                  size="mini"
+                  @click="deleteParams(scope.row)"
                   >删除</el-button
                 >
               </template>
@@ -297,6 +305,31 @@ export default {
         this.getParamsData()
         this.editDialogVisible = false
       })
+    },
+    // 删除参数
+    async deleteParams(scopeRow) {
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该参数, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(error => error)
+
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除')
+      }
+
+      const { data: res } = await this.$http.delete(
+        `categories/${this.cateId}/attributes/${scopeRow.attr_id}`
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除参数失败')
+      }
+      this.$message.success('删除参数成功')
+      this.getParamsData()
     }
   },
   computed: {
