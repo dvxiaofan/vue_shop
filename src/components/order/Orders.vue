fedc715983dc49013c6413d86b702857b7f56dab
xiaofan <1353815633@qq.com>
2020-04-09 15:58:42 +0800

    获取显示订单列表

--- src/components/order/Orders.vue
+++ src/components/order/Orders.vue
@@ -1,11 +1,98 @@
 <template>
   <div>
-    <h3>订单管理页面组件</h3>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getOrdersList"
+          >
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="getOrdersList"
+            ></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+
+      <!-- 订单列表 -->
+      <el-table :data="ordersList" border stripe>
+        <el-table-column label="#" type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column
+          label="订单价格"
+          prop="order_price"
+          width="95px"
+        ></el-table-column>
+        <el-table-column
+          label="是否付款"
+          prop="order_pay"
+          width="90px"
+        ></el-table-column>
+        <el-table-column
+          label="是否发货"
+          prop="is_send"
+          width="90px"
+        ></el-table-column>
+        <el-table-column label="下单时间" prop="create_time" width="150px">
+          <template slot-scope="scope">
+            {{ scope.row.create_time | dateFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+          <el-button
+            type="success"
+            icon="el-icon-s-flag"
+            size="mini"
+          ></el-button>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      ordersList: [],
+      total: 0
+    }
+  },
+  created() {
+    this.getOrdersList()
+  },
+  methods: {
+    async getOrdersList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取订单列表失败')
+      }
+
+      console.log('order: ', res.data)
+      this.ordersList = res.data.goods
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
